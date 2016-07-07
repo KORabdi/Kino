09b7e5680f1807e23e3aa8b789b4fc46a06222a8
Konstantin <kostantin.k@seznam.cz>
2016-07-07 22:05:03 +0200

    Adding isset

--- app/model/UserModel.php
+++ app/model/UserModel.php
@@ -26,7 +26,7 @@ class UserModel extends BaseModel{
 	}
 	
 	public function ixExisting($param,$value){
-		if($this->database->table('users')->where($param, $value)->get(1)){
+		if(isset($this->database->table('users')->where($param, $value)->get(1))){
 			return TRUE;
 		}else{
 			return FALSE;
