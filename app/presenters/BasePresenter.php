c0ab36eae9cb0de447301eb4cae2f75fbec3dda2
KORabdi <kostantin.k@seznam.cz>
2016-07-13 18:21:16 +0200

    Deleted unused/remade function

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -8,8 +8,8 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter
 {
     protected $user;
     protected $httpRequest;
-    protected static $user_salt = "AEcx199opQ";
     protected $database;
+    protected $requestValidation;
     
     public function __construct(Nette\Database\Connection $database, Nette\Security\User $storage, Nette\Http\Request $url)
     {
@@ -21,29 +21,11 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter
     	$this->sendResponse(new Nette\Application\Responses\JsonResponse($movie));
     }
     
-    public function checkPost(){
-        if($this->httpRequest->getMethod() != 'POST'){
-            sendAPIError('Wrong method');
-        }
-    }
-        
+    //TODO: Send API Error
+    
     public function checkUserLogin(){
         if (!$this->user->isLoggedIn()) {
             sendAPIError('User is not logged');
         }
     }
-    
-    public function getRequestParam($name,$check = array()){
-        $param = $this->httpRequest->getPost($name);
-        if(is_null($param) && array_key_exists('empty', $check)){
-            sendAPIError('Param '.$name.' is empty');
-        }
-        if(is_numeric($param) && array_key_exists('int', $check)){
-            sendAPIError('Param '.$name.'is not number');
-        }
-        if(is_string($param) && array_key_exists('string', $check)){
-            sendAPIError('Param '.$name.'is not string');
-        }
-        return $param;
-    }   
 }
